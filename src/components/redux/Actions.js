9d5463b8ee2281f7102168b8a3c2a2f6999cf37c
AmoGlobine <dennisaubryy@gmail.com>
2024-08-07 21:46:46 +0200

    redux test action

---
+++ src/components/redux/Actions.js
@@ -0,0 +1,25 @@
+export const LOGIN_SUCCESS = "LOGIN_SUCCESS";
+export const LOGIN_FAIL = "LOGIN_FAIL";
+export const LOGOUT = "LOGOUT";
+
+import { LOGIN_SUCCESS, LOGIN_FAIL, LOGOUT } from "./type.actions";
+
+export const loginSuccess = (token) => {
+    return {
+        type: LOGIN_SUCCESS,
+        payload: token,
+    }
+}
+
+export const loginFailed = (error) => {
+    return {
+        type: LOGIN_FAIL,
+        payload: error,
+    }
+}
+
+export const logout = () => {
+    return {
+        type: LOGOUT,
+    }
+} 
